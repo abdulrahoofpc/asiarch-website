fb393fda0b42b2ac6026aaa00f5467cee6198b35
*abdul <raufpc1998@gamil.com>
2024-06-07 12:04:45 +0530

    first asiarch

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// function resize(){
+
+// }
+
+// $(document).ready(function(){
+//     resize();
+// });
+  
+// $(window).resize(function(){
+//     resize();
+// });
+
+// $(window).on('load',function(){
+//     resize();
+// });
+
+
+
+// our work js-------------------------------------
+
+
